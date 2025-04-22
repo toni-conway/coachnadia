e9c6ff5de16ea3290581d9ee4e71f52c81f18be6
Toni <153367053+toni-conway@users.noreply.github.com>
2025-04-22 21:54:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -494,8 +494,8 @@ def process_location(dp_reco_df, tc_reco_df):
             if len(dp_reco_df) == 0:
                 st.info(f"No Developmental Pediatrician Found")
             else:
-                st.dataframe(dp_reco_df[['Developmental Pediatrician','Address','Contact']], hide_index=True)
-
+                #st.dataframe(dp_reco_df[['Developmental Pediatrician','Address','Contact']], hide_index=True)
+                t.info(dp_reco_df)
         st.info(tc_reco_df)
         
         if st.session_state.t_show == True:
