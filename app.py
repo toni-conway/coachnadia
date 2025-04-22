b8350c7e626f760b5f333d0b49e774110fbbae7e
Toni <153367053+toni-conway@users.noreply.github.com>
2025-04-22 20:39:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -487,7 +487,7 @@ def demographics_questionnaire():
 
         B1 = st.number_input("B1. What age is your child (in months) [0-48]?", 0, 48, help="Select the age of your child in months.")
         st.markdown("CHECK 1B")
-        # B2 = st.radio(label="B2. What is your child's gender?", options=["Male", "Female"], help="Choose the gender of your child.", horizontal=True, index=None)
+        B2 = st.radio("B2. What is your child's gender?", ["Male", "Female"], help="Choose the gender of your child.", horizontal=True, index=None)
         
         st.markdown("CHECK 2X")
         #B3 = st.radio("B3. What is your child's ethnicity?", ["Asian", "Black", "Hispanic", "Latino", "Middle Eastern", "Native Indian", "Pacifica", "South Asian", "White European", "Mixed", "Others"],
