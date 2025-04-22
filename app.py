ac9b4a02f74d36df3c2c74e380e3ce1c6eab821f
Toni <153367053+toni-conway@users.noreply.github.com>
2025-04-22 21:45:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -496,6 +496,8 @@ def process_location(dp_reco_df, tc_reco_df):
             else:
                 st.dataframe(dp_reco_df[['Developmental Pediatrician','Address','Contact']], hide_index=True)
 
+        st.dataframe(tc_reco_df)
+        
         if st.session_state.t_show == True:
             if len(tc_reco_df) == 0:
                 st.info(f"No Therapy Center Found")
